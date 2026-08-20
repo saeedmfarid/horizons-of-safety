32b8ee7f3095dd12a9b17ffc73d7609e065657a7
Saeed M. Farid <saeed.m.farid@gmail.com>
2026-08-20 20:37:13 +0330

    Add files vvia upload

---
+++ docs/js/mathjax.js
@@ -0,0 +1,9 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"], ["$", "$"]],
+    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+    processEscapes: true
+  },
+  options: { ignoreHtmlClass: ".*", processHtmlClass: "arithmatex" }
+};
+document$.subscribe(() => { MathJax.typesetPromise(); });
